3681b67641edca182d22c77372012711be3197fa
Serialbocks <john@serialbocks.com>
2023-03-26 17:10:30 -0400

    Use a better detection method

--- AcrSolver.cs
+++ AcrSolver.cs
@@ -3,11 +3,12 @@ using System.Drawing.Imaging;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
+using System.Drawing;
 
 namespace AcrSolver
 {
@@ -23,45 +24,93 @@ namespace AcrSolver
             uxStatus.AppendText(text + "\r\n");
         }
 
-        private bool isPixelButton(Bitmap screenshot, double xPercentage, double yPercentage)
+        private List<OpenCvSharp.Point> RunTemplateMatch(string reference, string template)
         {
-            var seatX = (int)(xPercentage * screenshot.Width);
-            var seatY = (int)(yPercentage * (screenshot.Height - topMargin) + topMargin);
-            var seatPixel = screenshot.GetPixel(seatX, seatY);
-            return seatPixel.R < 50 && seatPixel.G < 50 && seatPixel.B < 50;
+            var matches = new List<OpenCvSharp.Point>();
+            using (Mat refMat = new Mat(reference))
+            using (Mat tplMat = new Mat(template))
+            using (Mat res = new Mat(refMat.Rows - tplMat.Rows + 1, refMat.Cols - tplMat.Cols + 1, MatType.CV_32FC1))
+            {
+                //Convert input images to gray
+                Mat gref = refMat.CvtColor(ColorConversionCodes.BGR2GRAY);
+                Mat gtpl = tplMat.CvtColor(ColorConversionCodes.BGR2GRAY);
+
+                Cv2.MatchTemplate(gref, gtpl, res, TemplateMatchModes.CCoeffNormed);
+                Cv2.Threshold(res, res, 0.8, 1.0, ThresholdTypes.Tozero);
+
+                while (true)
+                {
+                    double minval, maxval, threshold = 0.8;
+                    OpenCvSharp.Point minloc, maxloc;
+                    Cv2.MinMaxLoc(res, out minval, out maxval, out minloc, out maxloc);
+
+                    if (maxval >= threshold)
+                    {
+                        //Setup the rectangle to draw
+                        var point = new OpenCvSharp.Point(maxloc.X, maxloc.Y);
+                        Rect r = new Rect(point, new OpenCvSharp.Size(tplMat.Width, tplMat.Height));
+                        matches.Add(point);
+
+                        //Draw a rectangle of the matching area
+                        Cv2.Rectangle(refMat, r, Scalar.LimeGreen, 2);
+
+                        //Fill in the res Mat so you don't find the same area again in the MinMaxLoc
+                        Rect outRect;
+                        Cv2.FloodFill(res, maxloc, new Scalar(0), out outRect, new Scalar(0.1), new Scalar(1.0));
+                    }
+                    else
+                        break;
+                }
+
+                return matches;
+            }
         }
 
         private int FindButton(Bitmap screenshot)
         {
-            if(isPixelButton(screenshot, 0.45824, 0.70448))
+            var matches = RunTemplateMatch("test.jpg", "button.jpg");
+
+            if(matches.Count == 0)
             {
-                return 1;
-            }
-            if(isPixelButton(screenshot, 0.28266, 0.60299))
-            {
-                return 2;
-            }
-            if (isPixelButton(screenshot, 0.17452, 0.35522))
-            {
-                return 3;
-            }
-            if (isPixelButton(screenshot, 0.59422, 0.27612))
-            {
-                return 4;
-            }
-            if (isPixelButton(screenshot, 0.76660, 0.28955))
-            {
-                return 5;
-            }
-            if (isPixelButton(screenshot, 0.69058, 0.69581))
-            {
-                return 6;
+                return -1;
             }
 
-            return -1;
+            var match = matches[0];
+            var third = screenshot.Width / 3;
+            if(match.Y > screenshot.Height / 2)
+            {
+                // Seat 1, 2, or 6
+                if(match.X < third)
+                {
+                    return 2;
+                }
+                else if(match.X > (2 * third))
+                {
+                    return 6;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else
+            {
+                // seat 3, 4, or 5
+                if (match.X < third)
+                {
+                    return 3;
+                }
+                else if (match.X > (2 * third))
+                {
+                    return 5;
+                }
+                else
+                {
+                    return 4;
+                }
+            }
         }
 
-        const int topMargin = 26;
         private void uxCapture_Click(object sender, EventArgs e)
         {
             var screenshot = Screenshot.PrintWindow();
@@ -70,6 +119,8 @@ namespace AcrSolver
                 WriteStatusLine("Could not find game window");
                 return;
             }
+
+            screenshot.Save("test.jpg", ImageFormat.Jpeg);
 
             // Find the button!
             var buttonSeat = FindButton(screenshot);
