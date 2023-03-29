49ef7a7328c583dea5e01538164a3db8ef88e560
Serialbocks <john@serialbocks.com>
2023-03-29 15:17:25 -0400

    Add player UI

--- AcrSolver.Designer.cs
+++ AcrSolver.Designer.cs
@@ -32,12 +32,30 @@ namespace AcrSolver
             this.uxCapture = new System.Windows.Forms.Button();
             this.uxStatus = new System.Windows.Forms.TextBox();
             this.uxClear = new System.Windows.Forms.Button();
+            this.uxSeat1Cards = new System.Windows.Forms.Label();
+            this.uxSeat1Stack = new System.Windows.Forms.Label();
+            this.uxSeat1Bet = new System.Windows.Forms.Label();
+            this.uxSeat2Cards = new System.Windows.Forms.Label();
+            this.uxSeat2Stack = new System.Windows.Forms.Label();
+            this.uxSeat2Bet = new System.Windows.Forms.Label();
+            this.uxSeat3Bet = new System.Windows.Forms.Label();
+            this.uxSeat3Stack = new System.Windows.Forms.Label();
+            this.uxSeat3Cards = new System.Windows.Forms.Label();
+            this.uxSeat4Bet = new System.Windows.Forms.Label();
+            this.uxSeat4Stack = new System.Windows.Forms.Label();
+            this.uxSeat4Cards = new System.Windows.Forms.Label();
+            this.uxSeat5Bet = new System.Windows.Forms.Label();
+            this.uxSeat5Stack = new System.Windows.Forms.Label();
+            this.uxSeat5Cards = new System.Windows.Forms.Label();
+            this.uxSeat6Bet = new System.Windows.Forms.Label();
+            this.uxSeat6Stack = new System.Windows.Forms.Label();
+            this.uxSeat6Cards = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // uxCapture
             // 
             this.uxCapture.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.uxCapture.Location = new System.Drawing.Point(192, 420);
+            this.uxCapture.Location = new System.Drawing.Point(192, 422);
             this.uxCapture.Name = "uxCapture";
             this.uxCapture.Size = new System.Drawing.Size(75, 23);
             this.uxCapture.TabIndex = 0;
@@ -47,22 +65,21 @@ namespace AcrSolver
             // 
             // uxStatus
             // 
-            this.uxStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.uxStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.uxStatus.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.uxStatus.Location = new System.Drawing.Point(12, 12);
+            this.uxStatus.Location = new System.Drawing.Point(560, 12);
             this.uxStatus.Multiline = true;
             this.uxStatus.Name = "uxStatus";
             this.uxStatus.ReadOnly = true;
             this.uxStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.uxStatus.Size = new System.Drawing.Size(434, 395);
+            this.uxStatus.Size = new System.Drawing.Size(229, 397);
             this.uxStatus.TabIndex = 1;
             // 
             // uxClear
             // 
             this.uxClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.uxClear.Location = new System.Drawing.Point(274, 420);
+            this.uxClear.Location = new System.Drawing.Point(274, 422);
             this.uxClear.Name = "uxClear";
             this.uxClear.Size = new System.Drawing.Size(75, 23);
             this.uxClear.TabIndex = 2;
@@ -70,11 +87,191 @@ namespace AcrSolver
             this.uxClear.UseVisualStyleBackColor = true;
             this.uxClear.Click += new System.EventHandler(this.uxClear_Click);
             // 
+            // uxSeat1Cards
+            // 
+            this.uxSeat1Cards.AutoSize = true;
+            this.uxSeat1Cards.Location = new System.Drawing.Point(252, 329);
+            this.uxSeat1Cards.Name = "uxSeat1Cards";
+            this.uxSeat1Cards.Size = new System.Drawing.Size(39, 15);
+            this.uxSeat1Cards.TabIndex = 3;
+            this.uxSeat1Cards.Text = "Ad Kd";
+            // 
+            // uxSeat1Stack
+            // 
+            this.uxSeat1Stack.AutoSize = true;
+            this.uxSeat1Stack.Location = new System.Drawing.Point(252, 354);
+            this.uxSeat1Stack.Name = "uxSeat1Stack";
+            this.uxSeat1Stack.Size = new System.Drawing.Size(40, 15);
+            this.uxSeat1Stack.TabIndex = 4;
+            this.uxSeat1Stack.Text = "$15.56";
+            // 
+            // uxSeat1Bet
+            // 
+            this.uxSeat1Bet.AutoSize = true;
+            this.uxSeat1Bet.Location = new System.Drawing.Point(257, 294);
+            this.uxSeat1Bet.Name = "uxSeat1Bet";
+            this.uxSeat1Bet.Size = new System.Drawing.Size(34, 15);
+            this.uxSeat1Bet.TabIndex = 5;
+            this.uxSeat1Bet.Text = "$1.00";
+            // 
+            // uxSeat2Cards
+            // 
+            this.uxSeat2Cards.AutoSize = true;
+            this.uxSeat2Cards.Location = new System.Drawing.Point(50, 294);
+            this.uxSeat2Cards.Name = "uxSeat2Cards";
+            this.uxSeat2Cards.Size = new System.Drawing.Size(70, 15);
+            this.uxSeat2Cards.TabIndex = 6;
+            this.uxSeat2Cards.Text = "HAS CARDS";
+            // 
+            // uxSeat2Stack
+            // 
+            this.uxSeat2Stack.AutoSize = true;
+            this.uxSeat2Stack.Location = new System.Drawing.Point(59, 313);
+            this.uxSeat2Stack.Name = "uxSeat2Stack";
+            this.uxSeat2Stack.Size = new System.Drawing.Size(40, 15);
+            this.uxSeat2Stack.TabIndex = 7;
+            this.uxSeat2Stack.Text = "$10.50";
+            // 
+            // uxSeat2Bet
+            // 
+            this.uxSeat2Bet.AutoSize = true;
+            this.uxSeat2Bet.Location = new System.Drawing.Point(120, 257);
+            this.uxSeat2Bet.Name = "uxSeat2Bet";
+            this.uxSeat2Bet.Size = new System.Drawing.Size(34, 15);
+            this.uxSeat2Bet.TabIndex = 8;
+            this.uxSeat2Bet.Text = "$1.00";
+            // 
+            // uxSeat3Bet
+            // 
+            this.uxSeat3Bet.AutoSize = true;
+            this.uxSeat3Bet.Location = new System.Drawing.Point(122, 128);
+            this.uxSeat3Bet.Name = "uxSeat3Bet";
+            this.uxSeat3Bet.Size = new System.Drawing.Size(34, 15);
+            this.uxSeat3Bet.TabIndex = 11;
+            this.uxSeat3Bet.Text = "$1.00";
+            // 
+            // uxSeat3Stack
+            // 
+            this.uxSeat3Stack.AutoSize = true;
+            this.uxSeat3Stack.Location = new System.Drawing.Point(50, 118);
+            this.uxSeat3Stack.Name = "uxSeat3Stack";
+            this.uxSeat3Stack.Size = new System.Drawing.Size(40, 15);
+            this.uxSeat3Stack.TabIndex = 10;
+            this.uxSeat3Stack.Text = "$10.50";
+            // 
+            // uxSeat3Cards
+            // 
+            this.uxSeat3Cards.AutoSize = true;
+            this.uxSeat3Cards.Location = new System.Drawing.Point(41, 99);
+            this.uxSeat3Cards.Name = "uxSeat3Cards";
+            this.uxSeat3Cards.Size = new System.Drawing.Size(70, 15);
+            this.uxSeat3Cards.TabIndex = 9;
+            this.uxSeat3Cards.Text = "HAS CARDS";
+            // 
+            // uxSeat4Bet
+            // 
+            this.uxSeat4Bet.AutoSize = true;
+            this.uxSeat4Bet.Location = new System.Drawing.Point(260, 77);
+            this.uxSeat4Bet.Name = "uxSeat4Bet";
+            this.uxSeat4Bet.Size = new System.Drawing.Size(34, 15);
+            this.uxSeat4Bet.TabIndex = 14;
+            this.uxSeat4Bet.Text = "$1.00";
+            // 
+            // uxSeat4Stack
+            // 
+            this.uxSeat4Stack.AutoSize = true;
+            this.uxSeat4Stack.Location = new System.Drawing.Point(260, 39);
+            this.uxSeat4Stack.Name = "uxSeat4Stack";
+            this.uxSeat4Stack.Size = new System.Drawing.Size(40, 15);
+            this.uxSeat4Stack.TabIndex = 13;
+            this.uxSeat4Stack.Text = "$10.50";
+            // 
+            // uxSeat4Cards
+            // 
+            this.uxSeat4Cards.AutoSize = true;
+            this.uxSeat4Cards.Location = new System.Drawing.Point(251, 20);
+            this.uxSeat4Cards.Name = "uxSeat4Cards";
+            this.uxSeat4Cards.Size = new System.Drawing.Size(70, 15);
+            this.uxSeat4Cards.TabIndex = 12;
+            this.uxSeat4Cards.Text = "HAS CARDS";
+            // 
+            // uxSeat5Bet
+            // 
+            this.uxSeat5Bet.AutoSize = true;
+            this.uxSeat5Bet.Location = new System.Drawing.Point(387, 118);
+            this.uxSeat5Bet.Name = "uxSeat5Bet";
+            this.uxSeat5Bet.Size = new System.Drawing.Size(34, 15);
+            this.uxSeat5Bet.TabIndex = 17;
+            this.uxSeat5Bet.Text = "$1.00";
+            // 
+            // uxSeat5Stack
+            // 
+            this.uxSeat5Stack.AutoSize = true;
+            this.uxSeat5Stack.Location = new System.Drawing.Point(440, 86);
+            this.uxSeat5Stack.Name = "uxSeat5Stack";
+            this.uxSeat5Stack.Size = new System.Drawing.Size(40, 15);
+            this.uxSeat5Stack.TabIndex = 16;
+            this.uxSeat5Stack.Text = "$10.50";
+            // 
+            // uxSeat5Cards
+            // 
+            this.uxSeat5Cards.AutoSize = true;
+            this.uxSeat5Cards.Location = new System.Drawing.Point(431, 67);
+            this.uxSeat5Cards.Name = "uxSeat5Cards";
+            this.uxSeat5Cards.Size = new System.Drawing.Size(70, 15);
+            this.uxSeat5Cards.TabIndex = 15;
+            this.uxSeat5Cards.Text = "HAS CARDS";
+            // 
+            // uxSeat6Bet
+            // 
+            this.uxSeat6Bet.AutoSize = true;
+            this.uxSeat6Bet.Location = new System.Drawing.Point(387, 257);
+            this.uxSeat6Bet.Name = "uxSeat6Bet";
+            this.uxSeat6Bet.Size = new System.Drawing.Size(34, 15);
+            this.uxSeat6Bet.TabIndex = 20;
+            this.uxSeat6Bet.Text = "$1.00";
+            // 
+            // uxSeat6Stack
+            // 
+            this.uxSeat6Stack.AutoSize = true;
+            this.uxSeat6Stack.Location = new System.Drawing.Point(449, 313);
+            this.uxSeat6Stack.Name = "uxSeat6Stack";
+            this.uxSeat6Stack.Size = new System.Drawing.Size(40, 15);
+            this.uxSeat6Stack.TabIndex = 19;
+            this.uxSeat6Stack.Text = "$10.50";
+            // 
+            // uxSeat6Cards
+            // 
+            this.uxSeat6Cards.AutoSize = true;
+            this.uxSeat6Cards.Location = new System.Drawing.Point(440, 294);
+            this.uxSeat6Cards.Name = "uxSeat6Cards";
+            this.uxSeat6Cards.Size = new System.Drawing.Size(70, 15);
+            this.uxSeat6Cards.TabIndex = 18;
+            this.uxSeat6Cards.Text = "HAS CARDS";
+            // 
             // uxMainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 455);
+            this.ClientSize = new System.Drawing.Size(801, 457);
+            this.Controls.Add(this.uxSeat6Bet);
+            this.Controls.Add(this.uxSeat6Stack);
+            this.Controls.Add(this.uxSeat6Cards);
+            this.Controls.Add(this.uxSeat5Bet);
+            this.Controls.Add(this.uxSeat5Stack);
+            this.Controls.Add(this.uxSeat5Cards);
+            this.Controls.Add(this.uxSeat4Bet);
+            this.Controls.Add(this.uxSeat4Stack);
+            this.Controls.Add(this.uxSeat4Cards);
+            this.Controls.Add(this.uxSeat3Bet);
+            this.Controls.Add(this.uxSeat3Stack);
+            this.Controls.Add(this.uxSeat3Cards);
+            this.Controls.Add(this.uxSeat2Bet);
+            this.Controls.Add(this.uxSeat2Stack);
+            this.Controls.Add(this.uxSeat2Cards);
+            this.Controls.Add(this.uxSeat1Bet);
+            this.Controls.Add(this.uxSeat1Stack);
+            this.Controls.Add(this.uxSeat1Cards);
             this.Controls.Add(this.uxClear);
             this.Controls.Add(this.uxStatus);
             this.Controls.Add(this.uxCapture);
@@ -90,6 +287,24 @@ namespace AcrSolver
         private System.Windows.Forms.Button uxCapture;
         private System.Windows.Forms.TextBox uxStatus;
         private System.Windows.Forms.Button uxClear;
+        private System.Windows.Forms.Label uxSeat1Cards;
+        private System.Windows.Forms.Label uxSeat1Stack;
+        private System.Windows.Forms.Label uxSeat1Bet;
+        private System.Windows.Forms.Label uxSeat2Cards;
+        private System.Windows.Forms.Label uxSeat2Stack;
+        private System.Windows.Forms.Label uxSeat2Bet;
+        private System.Windows.Forms.Label uxSeat3Bet;
+        private System.Windows.Forms.Label uxSeat3Stack;
+        private System.Windows.Forms.Label uxSeat3Cards;
+        private System.Windows.Forms.Label uxSeat4Bet;
+        private System.Windows.Forms.Label uxSeat4Stack;
+        private System.Windows.Forms.Label uxSeat4Cards;
+        private System.Windows.Forms.Label uxSeat5Bet;
+        private System.Windows.Forms.Label uxSeat5Stack;
+        private System.Windows.Forms.Label uxSeat5Cards;
+        private System.Windows.Forms.Label uxSeat6Bet;
+        private System.Windows.Forms.Label uxSeat6Stack;
+        private System.Windows.Forms.Label uxSeat6Cards;
     }
 }
 
